c2ea5b4657e0fa72364590fbfce14ef4cd995c93
betich <betich365@gmail.com>
2022-01-12 16:10:11 +0700

    :wave: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <main>
+        <h1>Next.js App</h1>
+        <p>
+          Generated using{" "}
+          <a href="https://github.com/betich/next-boilerplate" target="_blank" rel="noreferrer">
+            {"betich's"}
+          </a>{" "}
+          Next.js boilerplate template
+        </p>
+      </main>
+    </>
+  );
+};
+
+export default Home;
