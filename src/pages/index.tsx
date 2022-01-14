8e3d5310ec996a0c67031a7f6dcbca74d2e96b6e
betich <betich365@gmail.com>
2022-01-14 15:27:09 +0700

    close

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,20 +31,20 @@ const Home: NextPage<{ data: any }> = ({ data }) => {
       <main className="font-display py-8 w-full min-h-screen bg-[#373677] text-white flex flex-col items-center justify-center">
         <h1 className="py-4 font-semibold text-2xl">TUOPH2022 ZOOM Links</h1>
         <div className="flex flex-col sm:flex-row items-center justify-center sm:space-y-0 space-y-2 space-x-0 sm:space-x-4">
-          <div className="flex flex-col space-y-2">
-            <div className="flex flex-col bg-gray-300 rounded-lg px-4 bg-white text-black py-2">
-              <h1>ID: {data["sci-math"].id}</h1>
-              <h1>Password: {data["sci-math"].password}</h1>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={data["sci-math"].link}
-                className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 px-8 mt-2 shadow-sm bg-white text-black"
-              >
-                เข้า ZOOM วิทย์-คณิต
-              </a>
-            </div>
-            {element(data["arts-math"], "ศิลป์คำนวณ")}
+          {/* <div className="flex flex-col space-y-2"> */}
+          <div className="flex flex-col bg-gray-300 rounded-lg px-4 bg-white text-black py-2">
+            <h1>ID: {data["sci-math"].id}</h1>
+            <h1>Password: {data["sci-math"].password}</h1>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={data["sci-math"].link}
+              className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 px-8 mt-2 shadow-sm bg-white text-black"
+            >
+              เข้า ZOOM วิทย์-คณิต
+            </a>
+          </div>
+          {/* {element(data["arts-math"], "ศิลป์คำนวณ")}
             {element(data["arts-french"], "ศิลป์ฝรั่งเศส")}
             {element(data["arts-espanol"], "ศิลป์สเปน")}
           </div>
@@ -53,7 +53,7 @@ const Home: NextPage<{ data: any }> = ({ data }) => {
             {element(data["arts-japan"], "ศิลป์ญี่ปุ่น")}
             {element(data["arts-korea"], "ศิลป์เกาหลี")}
             {element(data["arts-china"], "ศิลป์จีน")}
-          </div>
+          </div> */}
         </div>
       </main>
     </>
