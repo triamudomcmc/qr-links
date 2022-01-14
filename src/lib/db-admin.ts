d2e94effd71c7f5ebab0e098fd8c7363a630f28f
pryter <peterphongpak@gmail.com>
2022-01-14 12:52:57 +0700

    Add id

--- src/lib/db-admin.ts
+++ src/lib/db-admin.ts
@@ -3,9 +3,14 @@ import { getDb } from "./firebase-admin";
 const db = getDb();
 
 export async function getZoomData() {
-  const ref = db.collection("zoomlinks").doc("data");
+  const ref = db.collection("zoomlinks")
   const doc = await ref.get();
-  const data = doc.data();
+  const data = {}
+
+  doc.docs.forEach((e) => {
+    // @ts-ignore
+    data[e.id] = e.data()
+  })
 
   if (!data) return null;
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,23 @@
 import type { NextPage } from "next";
 import { getZoomData } from "src/lib/db-admin";
 
+const element = (data: any, bname :string) => {
+    console.log(data)
+  return (
+      <div className="flex flex-col bg-gray-300 rounded-lg px-4 bg-white text-black py-2">
+        <h1>ID: {data.id}</h1>
+        <h1>Password: {data.password}</h1>
+        <a
+            target="_blank"
+            rel="noreferrer"
+            href={data.link}
+            className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 px-8 mt-2 shadow-sm bg-white text-black"
+        >
+          {bname}
+        </a>
+      </div>
+  )
+}
 const Home: NextPage<{ data: any }> = ({ data }) => {
   if (!data)
     return (
@@ -14,73 +31,28 @@ const Home: NextPage<{ data: any }> = ({ data }) => {
       <main className="font-display py-8 w-full min-h-screen bg-[#373677] text-white flex flex-col items-center justify-center">
         <h1 className="py-4 font-semibold text-2xl">TUOPH2022 ZOOM Links</h1>
         <div className="flex flex-col sm:flex-row items-center justify-center space-x-4">
-          <div className="flex flex-col">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={data["sci-math"]}
-              className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 my-2 px-8 shadow-sm bg-white text-black"
-            >
-              วิทย์-คณิต
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={data["arts-math"]}
-              className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 my-2 px-8 shadow-sm bg-white text-black"
-            >
-              ศิลป์คำนวณ
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={data["arts-french"]}
-              className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 my-2 px-8 shadow-sm bg-white text-black"
-            >
-              ศิลป์ฝรั่งเศส
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={data["arts-espanol"]}
-              className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 my-2 px-8 shadow-sm bg-white text-black"
-            >
-              ศิลป์สเปน
-            </a>
+          <div className="flex flex-col space-y-2">
+            <div className="flex flex-col bg-gray-300 rounded-lg px-4 bg-white text-black py-2">
+              <h1>ID: {data["sci-math"].id}</h1>
+              <h1>Password: {data["sci-math"].password}</h1>
+              <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={data["sci-math"].link}
+                  className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 px-8 mt-2 shadow-sm bg-white text-black"
+              >
+                วิทย์-คณิต
+              </a>
+            </div>
+            {element(data["arts-math"], "ศิลป์คำนวณ")}
+            {element(data["arts-french"], "ศิลป์ฝรั่งเศส")}
+            {element(data["arts-espanol"], "ศิลป์สเปน")}
           </div>
-          <div className="flex flex-col">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={data["arts-deutsch"]}
-              className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 my-2 px-8 shadow-sm bg-white text-black"
-            >
-              ศิลป์เยอรมัน
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={data["arts-japan"]}
-              className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 my-2 px-8 shadow-sm bg-white text-black"
-            >
-              ศิลป์ญี่ปุ่น
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={data["arts-korean"]}
-              className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 my-2 px-8 shadow-sm bg-white text-black"
-            >
-              ศิลป์เกาหลี
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={data["arts-chinese"]}
-              className="rounded-full hover:bg-gray-100 transition-colors text-center w-[250px] py-4 my-2 px-8 shadow-sm bg-white text-black"
-            >
-              ศิลป์จีน
-            </a>
+          <div className="flex flex-col space-y-2">
+            {element(data["arts-deutsch"], "ศิลป์เยอรมัน")}
+            {element(data["arts-japan"], "ศิลป์ญี่ปุ่น")}
+            {element(data["arts-korea"], "ศิลป์เกาหลี")}
+            {element(data["arts-china"], "ศิลป์จีน")}
           </div>
         </div>
       </main>
